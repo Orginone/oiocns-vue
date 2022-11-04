87402eddad0971ac77d06b97c6460ee4d1a6937a
cui <cui_0714@163.com>
2022-11-04 11:11:13 +0800

    feature:首页重构

---
+++ src/components/protal/components/cent.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="center">
+    <div class="left">
+      <div class="L_top">
+        <p class="lsolid"></p>
+        <span class="csolid">快捷入口</span>
+        <p class="rsolid"></p>
+        <el-icon class="eidtIcon"><Edit /></el-icon>
+      </div>
+      <div class="L_bottom">
+        <div class="lb_col">
+            <el-card class="card" shadow="hover"> 
+              <el-icon>
+                <User />
+              </el-icon><span>Always</span> </el-card>
+              <el-card class="card" shadow="hover"> 
+              <el-icon>
+                <User />
+              </el-icon><span>Always</span> </el-card>
+              <el-card class="card" shadow="hover"> 
+              <el-icon>
+                <User />
+              </el-icon><span>Always</span> </el-card>
+              <el-card class="card" shadow="hover"> 
+              <el-icon>
+                <User />
+              </el-icon><span>Always</span> </el-card>
+              <el-card class="card" shadow="hover"> 
+              <el-icon>
+                <User />
+              </el-icon><span>Always</span> </el-card>
+              <el-card class="card" shadow="hover"> 
+              <el-icon>
+                <User />
+              </el-icon><span>Always</span> </el-card>
+        </div>
+      </div>
+    </div>
+    <div class="right">
+      <div class="R_top">
+        <p class="lsolid"></p>
+        <span class="csolid">常用应用</span>
+        <p class="rsolid"></p>
+        <el-icon class="eidtIcon"><Edit /></el-icon>
+      </div>
+      <div class="R_bottom">
+        <div class="rb_col">
+            <div class="card">
+              <span><img src="@/assets/img/app2.png" alt=""></span>
+              <span>资产监管平台</span>
+              <span>简单、高效、开放的监管工具</span>
+            </div>
+            <div class="card">
+              <span><img src="@/assets/img/app2.png" alt=""></span>
+              <span>资产监管平台</span>
+              <span>简单、高效、开放的监管工具</span>
+            </div>
+            <div class="card">
+              <span><img src="@/assets/img/app2.png" alt=""></span>
+              <span>资产监管平台</span>
+              <span>简单、高效、开放的监管工具</span>
+            </div>
+            <div class="card">
+              <span><img src="@/assets/img/app2.png" alt=""></span>
+              <span>资产监管平台</span>
+              <span>简单、高效、开放的监管工具</span>
+            </div>
+            <div class="card">
+              <span><img src="@/assets/img/app2.png" alt=""></span>
+              <span>资产监管平台</span>
+              <span>简单、高效、开放的监管工具</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/store/user'
+import { storeToRefs } from 'pinia'
+import { computed,onMounted,ref } from 'vue'
+
+const store = useUserStore()
+const { queryInfo } = storeToRefs(store)
+
+onMounted(() => {
+
+})
+  
+</script>
+
+<style lang='scss' scoped>
+
+.center {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  .left{
+    width: 30%;
+    background: white;
+    border-radius: 5px;
+    margin-right: 6px;
+    .eidtIcon{
+      margin-left: 20px;
+    }
+    .lsolid{
+      width: 80px;
+      margin-right: 15px;
+      border-bottom: 1px solid #ccc;
+    }
+    .rsolid{
+      width: 80px;
+      margin-left: 15px;
+      border-bottom: 1px solid #ccc;
+    }
+    .csolid{
+      font-size: 16px;
+      font-weight: bold;
+    }
+    .L_top{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 20px;
+    }
+    .L_bottom{
+      margin-top: 20px;
+    }
+    .lb_col{
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      .card{
+        margin: 5px 10px;
+        width: 25%;
+        display: flex;
+        justify-content: center;
+        span{
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+  .right{
+    width: 70%;
+    background: white;
+    border-radius: 5px;
+    .eidtIcon{
+      margin-left: 20px;
+    }
+    .lsolid{
+      width: 120px;
+      margin-right: 15px;
+      border-bottom: 1px solid #ccc;
+    }
+    .rsolid{
+      width: 120px;
+      margin-left: 15px;
+      border-bottom: 1px solid #ccc;
+    }
+    .csolid{
+      font-size: 16px;
+      font-weight: bold;
+    }
+    .R_top{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 20px;
+    }
+    .R_bottom{
+      margin-top: 20px;
+    }
+    .rb_col{
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      .card{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        span:nth-child(1) {
+          width: 60px;
+          height: 60px;
+          margin: 10px 0;
+          img{
+            width: 100%;
+            border-radius: 15px;
+            height: auto;
+          }
+        }
+        span:nth-child(2) {
+          font-size: 15px;
+          font-weight: bold;
+          margin: 5px 0;
+        }
+        span:nth-child(3) {
+          font-size: 12px;
+          color: #ccc;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/components/protal/components/head.vue
+++ src/components/protal/components/head.vue
@@ -1,161 +1,48 @@
 <template>
   <div class="header">
-    <div class="header-left">
-      <img class="header-left-img" src="@/assets/img/头像.png" alt="" />
-      <!-- <div class="header-left__avater">俞</div> -->
-      <div class="header-left-box">
-        <div class="header-left-box__top">{{ getTimeState() }} {{ queryInfo.name }}!</div>
-        <div class="header-left-box__btm">管理⼈员 | 本次登录 {{ getTime }}</div>
-      </div>
-    </div>
-    <div class="header-right">
-      <!-- <el-divider direction="vertical" style="height:40px;"/>
-      
-      <div class="header-right-box">
-        <div class="header-right-top">申请</div>
-        <el-link class="header-right-btm" @click="jumpApproval('2')" :underline="false" >
-            {{applyNum}}
-        </el-link>
-      </div> -->
-      <!-- <div class="divide"></div>
-      <div class="header-right-box">
-        <div class="header-right-top">团队内排名</div>
-        <div class="header-right-btm">
-          <div class="header-right-btm__btmText">/24</div>
-          <div>0</div>
-        </div>
-      </div>
-      <div class="divide"></div>
-      <div class="header-right-box">
-        <div class="header-right-top">项目访问</div>
-        <div class="header-right-btm">0</div>
-      </div> -->
-    </div>
+    <el-carousel indicator-position="none" height="330px">
+      <el-carousel-item v-for="item in state.imgData" :key="item" >
+        <img :src='item.img' alt="">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useUserStore } from '@/store/user'
 import { storeToRefs } from 'pinia'
-import { computed,onMounted,ref } from 'vue'
-import $services from '@/services'
-import { useRoute, useRouter } from 'vue-router'
+import { computed,onMounted,reactive } from 'vue'
+import banner0 from '@/assets/img/banner0.png'
+import banner1 from '@/assets/img/banner1.png'
+import banner2 from '@/assets/img/banner2.png'
+import banner3 from '@/assets/img/banner3.png'
+import banner4 from '@/assets/img/banner4.png'
 
 const store = useUserStore()
 const { queryInfo } = storeToRefs(store)
-
-const getTimeState = () => {
-  // 获取当前时间
-  let timeNow = new Date()
-  // 获取当前小时
-  let hours = timeNow.getHours()
-  // 设置默认文字
-  let state = ``
-  // 判断当前时间段
-  if (hours >= 0 && hours <= 10) {
-    state = `早上好,`
-  } else if (hours > 10 && hours <= 14) {
-    state = `中午好,`
-  } else if (hours > 14 && hours <= 18) {
-    state = `下午好,`
-  } else if (hours > 18 && hours <= 24) {
-    state = `晚上好,`
-  }
-  return state
-}
-const getTime = computed(() => {
-  let date = new Date()
-  let y = date.getFullYear() //年
-  let m: any = date.getMonth() + 1 //月，月是从0开始的所以+1
-  let d: any = date.getDate() //日
-  let h: any = date.getHours()
-  let min: any = date.getMinutes()
-  m = m < 10 ? '0' + m : m //小于10补0
-  d = d < 10 ? '0' + d : d //小于10补0
-  min = min < 10 ? '0' + min : min //小于10补0
-  return y + '-' + m + '-' + d + ' ' + h + ':' + min //返回时间形式yyyy-mm-dd
+const state = reactive({
+  imgData: [
+    {id: 1, img: banner0},
+    {id: 2, img: banner1},
+    {id: 3, img: banner2},
+    {id: 4, img: banner3},
+    {id: 5, img: banner4}
+  ]
 })
 onMounted(() => {
 
 })
-
-const router = useRouter()
-const jumpApproval = (type:string)=>{
- router.push({ path: '/cardDetail' ,query: {type:type,id: "0"}})
-}
   
 </script>
 
 <style lang='scss' scoped>
-
 .header {
   width: 100%;
   height: 100%;
-  // min-height: 90px;
-  background-color: var(--el-bg-color-overlay);
-  display: flex;
-  justify-content: space-between;
-  padding-right: 24px;
-  &-left {
-    display: flex;
-    align-items: center;
-    &-img {
-      margin-left: 20px;
-      margin-right: 20px;
-    }
-    &__avater {
-      margin-left: 20px;
-      text-align: center;
-      line-height: 60px;
-      width: 60px;
-      height: 60px;
-      border-radius: 50%;
-      background: rgb(35, 72, 211);
-      color: #fff;
-      font-size: 26px;
-      margin-right: 20px;
-    }
-    &-box {
-      margin-top: 10px;
-      &__top {
-        font-weight: 600;
-        font-size: 18px;
-        margin-bottom: 10px;
-      }
-      &__btm {
-        font-weight: 600;
-        font-size: 12px;
-        margin-bottom: 10px;
-        color: rgb(192, 192, 192);
-      }
-    }
-  }
-  &-right {
-    display: flex;
-    align-items: center;
-    &-box {
-      padding-right: 32px;
-      padding-left: 32px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-    }
-    &-top {
-      margin-bottom: 5px;
-      color: #909399;
-    }
-    &-btm {
-      display: flex;
-      font-size: 22px;
-      // font-weight: 600;
-      flex-direction: row-reverse;
-      // color: $colorBlueMain;
-      &__btmText {
-        font-size: 14px;
-        color: rgb(143, 143, 143);
-        line-height: 28px;
-      }
-    }
+  img{
+    width: 100%;
+    height: 100%;
+    display: block;
   }
 }
 </style>
