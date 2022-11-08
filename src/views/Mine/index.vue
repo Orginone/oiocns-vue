0037a3d13820425c8b803cc4583e409cc10cfcb7
âcgm <85298826@qq.com>
2022-11-08 10:50:09 +0800

    表格

--- src/views/Mine/index.vue
+++ src/views/Mine/index.vue
@@ -13,12 +13,18 @@
           </div>
         </div>
         <div class="userInfoBox">
-          <img src="/src/assets/img/头像.png" alt="" class="avatar">
+          <img src="/src/assets/img/头像.png" alt="" class="avatar" />
           <div class="showDetail">
             <div v-for="row in showDetail" class="showDetailRow">
-              <p v-for="item in row" class="detailItem" :style="{ width: item.width ? item.width : '300px' }">
+              <p
+                v-for="item in row"
+                class="detailItem"
+                :style="{ width: item.width ? item.width : '300px' }"
+              >
                 <b class="userLabel">{{ item.label }}：</b>
-                <span class="userShowValue">{{ getDescendantProp(queryInfo, item.field) || '-' }}</span>
+                <span class="userShowValue">{{
+                  getDescendantProp(queryInfo, item.field) || "-"
+                }}</span>
               </p>
             </div>
           </div>
@@ -26,7 +32,7 @@
         <!-- 更多信息 -->
         <div class="expan">
           <span @click="isExpan = !isExpan">
-            <b>{{ isExpan ? '收起' : '更多信息' }}</b>
+            <b>{{ isExpan ? "收起" : "更多信息" }}</b>
             <el-icon :class="{ isExpan }">
               <ArrowDownBold />
             </el-icon>
@@ -35,41 +41,141 @@
         <div class="userInfoBox" v-show="isExpan">
           <div class="showDetail">
             <div v-for="row in expanDetail" class="showDetailRow">
-              <p v-for="item in row" class="detailItem" :style="{ width: item.width ? item.width : '300px' }">
+              <p
+                v-for="item in row"
+                class="detailItem"
+                :style="{ width: item.width ? item.width : '300px' }"
+              >
                 <b class="userLabel">{{ item.label }}：</b>
-                <span class="userShowValue">{{ getDescendantProp(queryInfo, item.field) || '-' }}</span>
+                <span class="userShowValue">{{
+                  getDescendantProp(queryInfo, item.field) || "-"
+                }}</span>
               </p>
             </div>
           </div>
         </div>
       </div>
+      <diytab
+        :style="{ height: 300 + 'px', width: '100%' }"
+        ref="diyTable"
+        :hasTableHead="true"
+        :tableData="tableData"
+        :options="options"
+        :tableHead="tableHead"
+        @handleUpdate="handleUpdate"
+        @selectionChange="selectionChange"
+      >
+        <template #remark="scope">
+          <el-tooltip
+            :content="scope.row.remark"
+            placement="bottom"
+            effect="light"
+          >
+            <template #content>
+              <div class="tooltip-text" style="width: 300px">
+                {{ scope.row.remark }}
+              </div>
+            </template>
+            <div class="remark-text">
+              {{ scope.row.remark }}
+            </div>
+          </el-tooltip>
+        </template>
+      </diytab>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted, watch, reactive } from "vue";
-import Bucket from "@/module/cloud/bucket";
+import diytab from "@components/diyTable/index.vue";
 import { useUserStore } from "@/store/user";
 import { storeToRefs } from "pinia";
 import { getDescendantProp } from "@/utils/tools";
-import { ShowDetailRow } from './type'
+import { ShowDetailRow } from "./type";
 
 const store = useUserStore();
 const { queryInfo } = storeToRefs(store);
-// 是否展开更多信息
-const isExpan = ref(false)
+// #region 展示信息
+const isExpan = ref<boolean>(false);
+const diyTable = ref(null);
 // 显示配置
 const showDetail = reactive<ShowDetailRow[][]>([
-  [{ label: '姓名', field: 'team.name' }],
-  [{ label: '性别', field: 'name' }, { label: '邮箱', field: 'name' }, { label: '联系方式', field: 'name' }],
-  [{ label: '地址', field: 'name', width: '100%' }],
-])
+  [{ label: "姓名", field: "team.name" }],
+  [
+    { label: "性别", field: "name" },
+    { label: "邮箱", field: "name" },
+    { label: "联系方式", field: "name" },
+  ],
+  [{ label: "地址", field: "name", width: "100%" }],
+]);
 const expanDetail = reactive<ShowDetailRow[][]>([
-  [{ label: '职位', field: 'team.name' }, { label: '级别', field: 'team.name' }],
-  [{ label: '其他信息', field: 'name' }, { label: '其他信息', field: 'name' }],
-  [{ label: '其他信息', field: 'name' }, { label: '其他信息', field: 'name' }],
-])
+  [
+    { label: "职位", field: "team.name" },
+    { label: "级别", field: "team.name" },
+  ],
+  [
+    { label: "其他信息", field: "name" },
+    { label: "其他信息", field: "name" },
+  ],
+  [
+    { label: "其他信息", field: "name" },
+    { label: "其他信息", field: "name" },
+  ],
+]);
+// #endregion
+
+// #region 表格部分
+const pageStore = reactive({
+  currentPage: 1,
+  pageSize: 20,
+  total: 0,
+});
+const tableData = reactive([]);
+const checkList = reactive<any>([])
+const options = ref<any>({
+  checkBox: true,
+  order: true,
+  selectLimit: 1,
+  defaultSort: { prop: "createTime", order: "descending" },
+  treeProps: {
+    children: "children",
+    hasChildren: "hasChildren",
+  },
+});
+const tableHead = ref([
+  {
+    prop: "paymentType",
+    label: "付款方式",
+  },
+  {
+    prop: "price",
+    label: "价格",
+    name: "price",
+  },
+  {
+    prop: "status",
+    label: "状态",
+    name: "status",
+  },
+  {
+    prop: "createTime",
+    label: "创建时间",
+    name: "createTime",
+  },
+]);
+
+const handleUpdate = (page: any) => {
+  pageStore.currentPage = page.currentPage;
+  pageStore.pageSize = page.pageSize;
+  remoteMethod();
+};
+
+const remoteMethod = () => {};
+const selectionChange = (val: any) => {
+  checkList.value = val;
+};
+// #endregion
 
 const changePass = () => {
   console.log("changePass");
@@ -79,11 +185,17 @@ const resetPass = () => {
 };
 
 onMounted(() => {
-  store.getQueryInfo(store.userToken)
-})
-
+  store.getQueryInfo(store.userToken);
+  remoteMethod();
+});
 </script>
 <style lang="scss" scoped>
+.zhanweibr {
+  height: 63px;
+  margin-bottom: 5px;
+  background: #fff;
+}
+
 .mine {
   background: #fff;
   border-radius: 5px;
@@ -92,6 +204,7 @@ onMounted(() => {
   .titleBox {
     display: flex;
     justify-content: space-between;
+    background: #fff;
 
     .title {
       font-weight: 600;
@@ -154,7 +267,7 @@ onMounted(() => {
       vertical-align: revert;
       font-size: 10px;
       margin-left: 8px;
-      transition: transform .3s;
+      transition: transform 0.3s;
 
       &.isExpan {
         transform: rotateX(180deg);
@@ -178,12 +291,6 @@ onMounted(() => {
   .zhanweiright {
     padding: 5px 8px;
     flex: 1;
-
-    .zhanweibr {
-      height: 63px;
-      margin-bottom: 5px;
-      background: #fff;
-    }
   }
 }
 </style>
