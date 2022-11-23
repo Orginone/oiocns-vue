80d803bf2ea6b34ce8e3bb922bba5f8ce47a07c9
cui <cui_0714@163.com>
2022-11-23 17:39:54 +0800

    feat: 待办调整

--- src/views/Service/shop/index.vue
+++ src/views/Service/shop/index.vue
@@ -2,9 +2,8 @@
 <template>
   <div class="thing">
     <div class="content">
-      <div>
+      <!-- <div>
         <el-menu
-          v-if="!whiteList.includes(activeIndex)"
           :default-active="flowActive"
           class="el-menu"
           style="height: 40px; padding-left: 20px; margin-bottom: 20px"
@@ -16,10 +15,10 @@
           <el-menu-item index="3">我发起的</el-menu-item>
           <el-menu-item index="4">抄送我的</el-menu-item>
         </el-menu>
-      </div>
+      </div> -->
       
-      <div class="tab-list"  v-if="whiteList.includes(activeIndex)">
-        <div v-if="activeIndex == '1-3'">
+      <div class="tab-list">
+        <div>
           <DiyTable
           ref="diyTable"
           :hasTitle="false"
@@ -27,7 +26,7 @@
           :options="options"
           :hasTableHead="true"
           :tableData="state.approvalList"
-          :tableHead="state.tableHead"
+          :tableHead="tableHead"
         >
           <template #slot-tabs>
             <el-tabs v-model="activeName" style="width: 100%" @tabClick="shopClick">
@@ -69,66 +68,19 @@
           </DiyTable>
         </div>
       </div>
-       <div class="tab-list" v-else>
-        <DiyTable
-          class="diytable"
-          ref="diyTable"
-          :hasTableHead="false"
-          :tableData="tableData"
-          :options="options"
-          :tableHead="tableHead"
-        >
-          <template #productId="scope">
-            {{chat.getName(scope.row?.flowInstance?.flowRelation?.productId||scope.row?.flowTask?.flowInstance?.flowRelation?.productId||scope.row?.flowRelation?.productId)}}
-          </template>
-          <template #target.name="scope">
-            {{chat.getName(scope.row.createUser)}}
-          </template>
-          <template #content="scope">
-            {{scope.row?.flowInstance?.content || scope?.row?.flowTask?.flowInstance?.content || scope?.row?.content}}
-          </template>
-          <template #status="scope">
-            <div v-if="scope.row.status >= 0 && scope.row.status < 100">待批</div>
-            <div v-else-if="scope.row.status >= 100 && scope.row.status < 200">
-              <div v-if="scope.row?.flowTask?.flowNode?.nodeType=='审批'">已通过</div>
-              <div v-else-if="scope.row?.flowTask?.flowNode?.nodeType=='抄送'">已查阅</div>
-              <div v-else>已通过</div>
-            </div>
-            <div v-else>已拒绝</div>
-          </template>
-          <template #option="scope">
-            <el-dropdown>
-              <span class="el-dropdown-link">
-                ···
-              </span>
-              <template #dropdown>
-                <el-dropdown-menu>
-                  <el-dropdown-item v-if="flowActive == '1'" @click="showDetail(scope.row,1)">审核申请</el-dropdown-item>
-                  <el-dropdown-item v-if="flowActive == '2'" @click="showDetail(scope.row,1)">查看详情</el-dropdown-item>
-                  <el-dropdown-item v-if="flowActive == '3'" @click="showDetail(scope.row,1)">查看详情</el-dropdown-item>
-                  <el-dropdown-item v-if="flowActive == '4'" @click="showDetail(scope.row,1)">查看详情</el-dropdown-item>
-                  <el-dropdown-item v-if="flowActive == '3'" @click="showDetail(scope.row,3)">撤销</el-dropdown-item>
-                  <el-dropdown-item v-if="flowActive == '4'" @click="showDetail(scope.row,4)">阅读</el-dropdown-item>
-                </el-dropdown-menu>
-              </template>
-            </el-dropdown>
-          </template>
-        </DiyTable>
-      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
   import $services from '@/services'
-  import { ref, onMounted, reactive, getCurrentInstance } from 'vue'
+  import { ref, onMounted, reactive, nextTick, getCurrentInstance } from 'vue'
   import { storeToRefs } from 'pinia'
   import { useUserStore } from '@/store/user'
   import { useRoute,useRouter } from 'vue-router'
   import DiyTable from '@/components/diyTable/index.vue'
   import { ElMessage, ElMessageBox } from 'element-plus'
   import type { TabsPaneContext } from 'element-plus'
-  import menuItem from '@/components/menuItem/index.vue'
   import { chat } from '@/module/chat/orgchat'
   
   import thingServices from '@/module/flow/thing'
@@ -142,7 +94,8 @@
   const { workspaceData } = storeToRefs(store)
   var tableData = ref<any>([{id:123,flowInstance:{}}])
   const diyTable = ref(null)
-  const tableHead =ref<any>(ThingServices.flowHead) ;
+  
+  const tableHead =ref<any>(ThingServices.state.searchJoinApplyHead);
   const options = {
     expandAll: false,
     checkBox: false,
@@ -158,55 +111,14 @@
     total: 0
   })
 
-  const showDetail = async (row: any,type:number) => {
-    if(type == 4){
-      $services.wflow.approvalTask({data:{id: row.id,status: 100,}}).then(async (res: ResultType) => {
-        ElMessage({
-          message: res.msg,
-          type: 'success'
-        })
-        ThingServices.whiteList = [];
-        await ThingServices.queryTask()
-        tableHead.value = ThingServices.flowHead;
-        tableData.value = ThingServices.copyList
-      })
-      
-    }else if(type ==3){
-      $services.wflow.deleteInstance({data:{id: row.id}}).then(async (res: ResultType) => {
-        ElMessage({
-          message: res.msg,
-          type: 'success'
-        })
-        ThingServices.whiteList = [];
-        await ThingServices.queryInstance()
-        tableHead.value = ThingServices.queryInstanceHead;
-        tableData.value =ThingServices.queryInstanceList
-      })
-     
-    }else{
-      let instanceId:string;
-      if(flowActive.value == '1'){
-        instanceId = row?.instanceId
-      }else if(flowActive.value == '2'){
-        instanceId = row?.flowTask?.instanceId
-      }else if(flowActive.value == '3'){
-        instanceId = row?.id
-      }else{
-        instanceId = row?.instanceId
-      }
-      router.push({ path: '/work/process', query: {id:row.id,type:flowActive.value,instanceId:instanceId}});
-    }
-  }
-
   const activeIndex = ref<string>('1')
   const flowActive = ref<string>('1')
   const activeId = ref<string>('0')
   const elmenus = ref(null);
   const activeName = ref('first') //商店tab
-
+  
   const state = reactive({
     approvalList: [],
-    tableHead: [],
   })
   const handleClose = (index:any) => {
     elmenus.value.open(index)
@@ -230,92 +142,33 @@
       searchManagerPublishApply()
     }
   }
-  var getList = async () => {
-    await ThingServices.getAllApproval('0')
-    tableData.value = ThingServices.approvalList
-  }
-
-  var getApplyList = async () => {
-    await ThingServices.getAllApply()
-    tableData.value = ThingServices.applyList
-  }
 
   //查询加入市场申请
   var searchJoinApply = async () => {
     await ThingServices.searchJoinApply()
     state.approvalList =ThingServices.state.searchJoinApplyList
-    state.tableHead =ThingServices.state.searchJoinApplyHead
+    tableHead.value = ThingServices.state.searchJoinApplyHead
   }
+  searchJoinApply()
   //查询加入市场审批
   const searchJoinApplyManager = async () => {
     await ThingServices.searchJoinApplyManager()
     state.approvalList =ThingServices.state.searchJoinApplyManagerList
-    state.tableHead =ThingServices.state.searchJoinApplyManagerHead
+    tableHead.value = ThingServices.state.searchJoinApplyManagerHead
   }
   //查询产品上架申请
   const searchPublishApply = async () => {
     await ThingServices.searchPublishApply()
     state.approvalList =ThingServices.state.searchPublishApplyList
-    state.tableHead =ThingServices.state.searchPublishApplyHead
+    tableHead.value = ThingServices.state.searchPublishApplyHead
   }
   //查询应用上架审批
   const searchManagerPublishApply = async () => {
     await ThingServices.searchManagerPublishApply()
     state.approvalList =ThingServices.state.searchManagerPublishApplyList
-    state.tableHead =ThingServices.state.searchManagerPublishApplyHead
-  }
-
-  const getWflow =async () => {
-    await ThingServices.queryTask()
-    tableHead.value = ThingServices.flowHead;
-    tableData.value =ThingServices.taskList
-    
-  }
-
-  const flowSelect = (key: string) => {
-    flowActive.value = key
-    flowSwitch(key)
-  }
-  const flowSwitch  = async (key: string) => {
-    if(key == '1'){
-      await ThingServices.queryTask()
-      tableHead.value = ThingServices.flowHead;
-      tableData.value =ThingServices.taskList
-    }else if(key =='2'){
-      await ThingServices.queryRecord()
-      tableHead.value = ThingServices.recordHead;
-      tableData.value =ThingServices.recordList
-    }else if(key =='3'){
-      await ThingServices.queryInstance()
-      tableHead.value = ThingServices.queryInstanceHead;
-      tableData.value =ThingServices.queryInstanceList
-    }else if(key =='4'){
-      tableHead.value = ThingServices.flowHead;
-      tableData.value = ThingServices.copyList
-    }
-  }
-
-  const handleNodeClick = (data: Array<any>) => {
-    console.log(data)
+    tableHead.value = ThingServices.state.searchManagerPublishApplyHead
   }
   
-  //审核拒绝
-  var joinRefse = (item: { id: '' }) => {
-    ElMessageBox.confirm('确定拒绝吗？', '提示', {
-      confirmButtonText: '确定',
-      cancelButtonText: '取消',
-      type: 'warning'
-    }).then( async () => {
-    const data = await ThingServices.joinRefuse(item.id)
-      if(data){
-        ElMessage({
-          message: '拒绝成功',
-          type: 'success'
-        })
-        handleSelect(activeIndex.value, [])
-      }
-    })
-  }
   //取消申请
   var cancelJoin = (item: { id: '' }) => {
     ElMessageBox.confirm('确定取消吗？', '提示', {
@@ -330,23 +183,6 @@
           type: 'success'
         })
         ThingServices.whiteList = [];
-        handleSelect(activeIndex.value, [])
-      }
-    })
-  }
-  //通过审核
-  var joinSuccess = (item: { id: '' }) => {
-    ElMessageBox.confirm('确定通过吗？', '提示', {
-      confirmButtonText: '确定',
-      cancelButtonText: '取消',
-      type: 'warning'
-    }).then( async () => {
-     const data = await ThingServices.joinSuccess(item.id)
-      if(data){
-        ElMessage({
-          message: '添加成功',
-          type: 'success'
-        })
         handleSelect(activeIndex.value, [])
       }
     })
@@ -420,31 +256,25 @@
       if (key == '1-3'){
         searchJoinApply()
       }
-    } else {
-      getWflow();
-     
-    }
+    } 
   }
 
   onMounted(() => {
-    ThingServices.whiteList = [];
-    const route = useRouter();
-    const selectType = route.currentRoute.value.query.type?'1-1':'2-1';
-    let id = route.currentRoute.value.query.id;
-    activeIndex.value = '1-1';
-    if (Array.isArray(id)) {
-      activeId.value = id[0];
-    } else {
-      activeId.value = id
-    }
-    console.log('selectType', selectType);
+    // ThingServices.whiteList = [];
+    // const route = useRouter();
+    // const selectType = route.currentRoute.value.query.type?'1-1':'2-1';
+    // let id = route.currentRoute.value.query.id;
+    // activeIndex.value = '1-1';
+    // if (Array.isArray(id)) {
+    //   activeId.value = id[0];
+    // } else {
+    //   activeId.value = id
+    // }
     
-    handleSelect(selectType, [])
+    // handleSelect(selectType, [])
   });
 
   instance?.proxy?.$Bus.on('selectBtn', (num) => {
-    console.log(num);
-    
     handleSelect(num, [])
   })
 
